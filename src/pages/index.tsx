77fddd39b966d2276c14dbdf7a38a57f6432b5a3
Thiago Alves <thiagowlol@gmail.com>
2021-04-11 17:12:46 -0300

    Static site generation para home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
 import Head from 'next/head';
-import { GetServerSideProps } from 'next';
+import { GetStaticProps } from 'next';
 import { SubscribeButton } from '../components/SubscribeButton/SubscribeButton';
 import styles from './home.module.scss'
 import { stripe } from '../services/stripe';
@@ -34,7 +34,7 @@ export default function Home( { product } : HomeProps ) {
   )
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
   const price = await stripe.prices.retrieve('price_1IeQQLGGXXcQrOMu0Rh39b3P')
 
   const product  = {
@@ -48,6 +48,7 @@ export const getServerSideProps: GetServerSideProps = async () => {
   return {
     props: {
       product,
-    }
+    },
+    revalidate: 60*60*24, // 24 hours
   }
 }
