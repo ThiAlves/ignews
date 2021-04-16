bffd97e62c8794a51294eb12db0b32f387375be9
Thiago Alves <thiagowlol@gmail.com>
2021-04-15 22:15:19 -0300

    Integração e configuração com o FaunaDB

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,7 @@
-import NextAuth from 'next-auth'
-import Providers from 'next-auth/providers'
+import NextAuth from "next-auth";
+import { query as q } from "faunadb";
+import Providers from "next-auth/providers";
+import { fauna } from "../../../services/fauna";
 
 export default NextAuth({
   // Configure one or more authentication providers
@@ -7,8 +9,20 @@ export default NextAuth({
     Providers.GitHub({
       clientId: process.env.GITHUB_CLIENT_ID,
       clientSecret: process.env.GITHUB_CLIENT_SECRET,
-      scope: 'read:user'
+      scope: "read:user",
     }),
-    // ...add more providers here
   ],
-})
+  callbacks: {
+    async signIn(user, account, profile) {
+      const { email } = user;
+
+      try {
+        await fauna.query(q.Create(q.Collection("users"), { data: { email } }));
+
+        return true;
+      } catch {
+        return false;
+      }
+    },
+  },
+});
