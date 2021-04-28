c5e9b43d3b2552f4ce34fb5f2549c7f5d6295bcb
Thiago Alves <thiagowlol@gmail.com>
2021-04-27 21:14:56 -0300

    Redirecionamento do usuário para checkout

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,5 +1,6 @@
 import { signIn, useSession } from 'next-auth/client';
 import { api } from '../../services/api';
+import { getStripeJs } from '../../services/stripe-js';
 import styles from './styles.module.scss'
 
 interface SubscribeButtonProps {
@@ -16,13 +17,15 @@ export function SubscribeButton( { priceId } : SubscribeButtonProps ) {
     }
 
     try {
-      const response = await api.post('/api/subscribe')
+      const response = await api.post('/subscribe')
 
       const { sessionId } = response.data;
 
-      
+      const stripe = await getStripeJs()
+
+      await stripe.redirectToCheckout({ sessionId })
     } catch (error) {
-      
+      alert(error.message);
     }
   }
 
